656c5ef067c4fb79aee7a62e0b93687dd3557f9c
Rajesh Subhankar <bunty2327.cse@gmail.com>
2019-08-24 02:36:19 +0200

    Add contract deployment

---
+++ api/config.js
@@ -0,0 +1,15 @@
+const config = {};
+
+config.FAUCET_PRIVATE_KEY = 'a0297f95a9565856b13f76feb4b2d09fea5514cd8e9ac7901165984463d7dedd';
+config.FAUCET_ADDRESS = '0x86005Cda50E5F22bD55d6B454b3765A499991e27';
+
+config.INFURA = {
+  ROPSTEN: 'https://ropsten.infura.io/v3/d7ef95bbf433420a96fa91c4a35b200b',
+  KOVAN: 'https://kovan.infura.io/v3/d7ef95bbf433420a96fa91c4a35b200b',
+  RINKEBY: 'https://rinkeby.infura.io/v3/d7ef95bbf433420a96fa91c4a35b200b',
+  GORLI: 'https://goerli.infura.io/v3/d7ef95bbf433420a96fa91c4a35b200b',
+}
+
+config.NETWORK = 'ropsten';
+
+module.exports = config;
--- api/routes/wallet.js
+++ api/routes/wallet.js
@@ -1,8 +1,86 @@
 var express = require('express');
-var router = express.Router();
+const Web3 = require('web3');
+const EthereumTx = require('ethereumjs-tx').Transaction;
+const config = require('../config');
+const walletABI = require('../abi/wallet.json');
+const testABI = require('../abi/test.json');
 
-router.get('/', (req, res, ext) => {
-  res.send('create wallet successful');
+var router = express.Router();
+const web3 = new Web3(config.INFURA.ROPSTEN);
+
+const privateKey = Buffer.from(
+  config.FAUCET_PRIVATE_KEY,
+  'hex',
+)
+
+/* Create raw prefund tx*/
+const createRawPrefundTx = async (toAddress) => {
+  const nonce = await web3.eth.getTransactionCount(config.FAUCET_ADDRESS);
+  const txParams = {
+  nonce: web3.utils.toHex(nonce),
+  gasPrice: '0x37E11D600',
+  gasLimit: web3.utils.toHex(90000),
+  to: toAddress,
+  value: web3.utils.toHex(web3.utils.toWei('0.05', 'ether')),
+}
+
+const tx = new EthereumTx(txParams, { chain: config.NETWORK});
+tx.sign(privateKey)
+const serializedTx = tx.serialize().toString('hex');
+
+return `0x${serializedTx}`;
+};
+
+/* Create raw contract deployment tx*/
+const createRawWalletDeployTx = async (eoa) => {
+  const walletContract = new web3.eth.Contract(testABI.abi);
+  const encodeAbi = walletContract.deploy({
+    data: testABI.bytecode,
+    // arguments: [eoa.address]
+  }).encodeABI();
+
+  const nonce = await web3.eth.getTransactionCount(eoa.address);
+  const txParams = {
+  nonce: web3.utils.toHex(nonce),
+  gasPrice: '0x37E11D600',
+  gasLimit: web3.utils.toHex(1990000),
+  data: encodeAbi,
+  }
+
+  const tx = new EthereumTx(txParams, { chain: config.NETWORK});
+  tx.sign(Buffer.from(eoa.privateKey.substring(2), 'hex'));
+  const serializedTx = tx.serialize().toString('hex');
+
+  return `0x${serializedTx}`;
+}
+
+/* Creates a new wallet */
+router.post('/', async (req, res, ext) => {
+  // create eoa
+  const eoa = web3.eth.accounts.create();
+  // console.log(eoa);
+
+  // prefund eoa
+  const signedTx = await createRawPrefundTx(eoa.address);
+  const prefundReceipt = await web3.eth.sendSignedTransaction(signedTx);
+
+  // deploy the smart contract wallet
+  const walletSignedTx = await createRawWalletDeployTx(eoa);
+  const walletReceipt = await web3.eth.sendSignedTransaction(walletSignedTx);
+
+  res.send({
+    eoaAddress: eoa.address,
+    eoaPrivateKey: eoa.privateKey,
+    walletAddress: walletReceipt.contractAddress
+  });
+
 });
+/*
+TODO:
+- Create an EOA - done
+- Prefund EOA with 0.05 eth - done
+- Deploy a smart contract wallet - donne
+- Return EOA private key and smart contract address - done
+*/
 
 module.exports = router;
